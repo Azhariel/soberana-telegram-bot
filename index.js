1755a21b44b475b901c469fe9232e78d05e86b1f
Azhariel <azhariel@gmail.com>
2022-04-17 19:07:37 -0300

    create ssl server

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 require('dotenv').config();
 const express = require('express');
+const crypto = require('crypto');
+const https = require('https');
+const fs = require('fs');
+
+const { TWITCH_SECRET_KEY } = process.env;
 
 const port = 4000;
 const app = express();
 
-
-// Up the server
 function main() {
-    app.listen(port, () => {
+    https.createServer(
+        {
+            key: fs.readFileSync('./ssl/key.pem'),
+            cert: fs.readFileSync('./ssl/cert.pem')
+        },
+        app
+    ).listen(port, () => {
         console.log(`🚀 Listening http://localhost:${port}`);
     })
 }
 
+app.get('/', (req, res) => {
+    res.send('All is right with the world');
+})
+
 app.get('/auth/google', (req, res) => {
     res.sendStatus(200);
 })
+
+
 
 main();
