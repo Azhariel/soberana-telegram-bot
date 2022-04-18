4cb0cb41b31fd69c82fc635a41902846ca7739a2
Azhariel <azhariel@gmail.com>
2022-04-18 01:47:56 -0300

    create https server, twitch eventsub handlers

--- index.js
+++ index.js
@@ -1,34 +1,127 @@
+// Imports
 require('dotenv').config();
 const express = require('express');
 const crypto = require('crypto');
 const https = require('https');
 const fs = require('fs');
+const { sendMessage } = require('./methods/telegram');
 
+// Twitch
 const { TWITCH_SECRET_KEY } = process.env;
+// Notification request headers
+const TWITCH_MESSAGE_ID = 'Twitch-Eventsub-Message-Id'.toLowerCase();
+const TWITCH_MESSAGE_TIMESTAMP = 'Twitch-Eventsub-Message-Timestamp'.toLowerCase();
+const TWITCH_MESSAGE_SIGNATURE = 'Twitch-Eventsub-Message-Signature'.toLowerCase();
+const MESSAGE_TYPE = 'Twitch-Eventsub-Message-Type'.toLowerCase();
+
+// Notification message types
+const MESSAGE_TYPE_VERIFICATION = 'webhook_callback_verification';
+const MESSAGE_TYPE_NOTIFICATION = 'notification';
+const MESSAGE_TYPE_REVOCATION = 'revocation';
+
+// Prepend this string to the HMAC that's created from the message
+const HMAC_PREFIX = 'sha256=';
+
 
 const port = 4000;
+const securePort = 443;
 const app = express();
+const secureApp = express();
 
+// Need raw message body for signature verification
+secureApp.use(express.raw({
+    type: 'application/json'
+}));
+
+// Start HTTP and HTTPS servers
 function main() {
+    app.listen(port, () => {
+        console.log(`🚀 Listening http://localhost:${port}`);
+    });
+
     https.createServer(
         {
             key: fs.readFileSync('./ssl/key.pem'),
             cert: fs.readFileSync('./ssl/cert.pem')
         },
-        app
-    ).listen(port, () => {
-        console.log(`🚀 Listening http://localhost:${port}`);
-    })
+        secureApp
+    ).listen(securePort, () => {
+        console.log(`🚀 Listening on https://localhost:${securePort}`);
+    });
 }
 
+// HTTPS SERVER
+secureApp.get('/', (req, res) => {
+    res.send(`God's in his heaven`)
+});
+
+// Build the message used to get the HMAC.
+function getHmacMessage(request) {
+    return (request.headers[TWITCH_MESSAGE_ID] +
+        request.headers[TWITCH_MESSAGE_TIMESTAMP] +
+        request.body);
+}
+
+// Get the HMAC.
+function getHmac(secret, message) {
+    return crypto.createHmac('sha256', secret)
+        .update(message)
+        .digest('hex');
+}
+
+// Verify whether our hash matches the hash that Twitch passed in the header.
+function verifyMessage(hmac, verifySignature) {
+    return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature));
+}
+
+secureApp.post('/eventsub', (req, res) => {
+    let secret = TWITCH_SECRET_KEY;
+    let message = getHmacMessage(req);
+    let hmac = HMAC_PREFIX + getHmac(secret, message);  // Signature to compare
+
+    if (true === verifyMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) {
+        console.log("signatures match");
+
+        // Get JSON object from body, so you can process the message.
+        let notification = JSON.parse(req.body);
+
+        if (MESSAGE_TYPE_NOTIFICATION === req.headers[MESSAGE_TYPE]) {
+            // TODO: Do something with the event's data. -> Send to telegram
+            sendMessage(`${notification.event.broadcaster_user_name} está online: https://twitch\\.tv/${notification.event.broadcaster_user_login}`);
+            console.log(`Event type: ${notification.subscription.type}`);
+            console.log(JSON.stringify(notification.event, null, 4));
+
+            res.sendStatus(204);
+        }
+        else if (MESSAGE_TYPE_VERIFICATION === req.headers[MESSAGE_TYPE]) {
+            res.set('content-type', 'text/plain');
+            res.status(200).send(notification.challenge);
+        }
+        else if (MESSAGE_TYPE_REVOCATION === req.headers[MESSAGE_TYPE]) {
+            res.sendStatus(204);
+
+            console.log(`${notification.subscription.type} notifications revoked!`);
+            console.log(`reason: ${notification.subscription.status}`);
+            console.log(`condition: ${JSON.stringify(notification.subscription.condition, null, 4)}`);
+        }
+        else {
+            res.sendStatus(204);
+            console.log(`Unknown message type: ${req.headers[MESSAGE_TYPE]}`);
+        }
+    }
+    else {
+        console.log('403');    // Signatures didn't match.
+        res.sendStatus(403);
+    }
+});
+
+// HTTP SERVER
 app.get('/', (req, res) => {
-    res.send('All is right with the world');
-})
+    res.send(`All's right with the world`);
+});
 
 app.get('/auth/google', (req, res) => {
     res.sendStatus(200);
-})
-
-
+});
 
 main();
