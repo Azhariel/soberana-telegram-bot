56f4d9d01b7a9997ff7a5222a1fd60199670c6ba
Azhariel <Azhariel@users.noreply.github.com>
2022-04-18 16:42:13 -0300

    create get and delete support functions

--- api/twitch.js
+++ api/twitch.js
@@ -14,12 +14,21 @@ const instance = axios.create({
             'Authorization': `Bearer ${TWITCH_TOKEN}`,
             'Client-Id': `${TWITCH_CLIENT_ID}`,
             'Content-Type': 'application/json'
+        },
+        get: {
+            'Authorization': `Bearer ${TWITCH_TOKEN}`,
+            'Client-Id': `${TWITCH_CLIENT_ID}`,
+            'Content-Type': 'application/json'
+        },
+        delete: {
+            'Authorization': `Bearer ${TWITCH_TOKEN}`,
+            'Client-Id': `${TWITCH_CLIENT_ID}`,
+            'Content-Type': 'application/json'
         }
     },
 })
 
 async function subscribeToStreamOnline(userId) {
-
     try {
         await instance.post('/eventsub/subscriptions', {
             "type": "stream.online",
@@ -38,4 +47,47 @@ async function subscribeToStreamOnline(userId) {
     }
 }
 
-subscribeToStreamOnline('150384125');
+async function getSubscribedEvents() {
+    try {
+        await instance.get('/eventsub/subscriptions').then(reponse => {
+            console.log(reponse.data.data.forEach(element => {
+                console.log(`Status: ${element.status} | ID: ${element.id} | UserID: ${element.condition.broadcaster_user_id}`)
+            }));
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getUsers(userName) {
+    try {
+        await instance.get(`/users?login=${userName}`).then(response => {
+            for (let userObject of response.data.data) {
+                console.log(`Login: ${userObject['login']} ID: ${userObject['id']} Profile Pic: ${userObject['profile_image_url']}`);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function deleteSubscribedEvent(eventId) {
+    try {
+        await instance.delete(`/eventsub/subscriptions?id=${eventId}`).then(reponse => {
+            console.log(reponse.status);
+        });
+    } catch (error) {
+        console.error(error.response.data);
+    }
+}
+
+// TODO: Outro GET para pegar o título da live (https://dev.twitch.tv/docs/api/reference#get-channel-information)
+
+// getUsers('azhariel&login=c0muninja&login=comuna_paint&login=dadosrevolucionarios&login=0froggy&login=gamerdeesquerda&login=historiapublica&login=historiatopia&login=lucaszawacki&login=ponzuzuju&login=vinnydays');
+// getSubscribedEvents();
+// subscribeToStreamOnline('40463426');
+// deleteSubscribedEvent('1ee178ba-2cb1-4275-b630-ae0a75dd513b');
+
+// soberanaIds = [146148785, 590931212, 539161835, 557225670, 40463426, 30865255, 694725890, 502441638, 127274602, 498425402, 49750261]
+// done 18/04/22
+// soberanaIds.forEach((cur, ind, arr) => { subscribeToStreamOnline(cur) });
